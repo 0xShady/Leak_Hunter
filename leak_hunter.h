87031ad919fe0ad61e5ffdebf681b215fb00c32b
0xShady <ael-fadi@student.1337.ma>
2021-11-07 20:49:45 +0100

    formating output

--- leak_hunter.h
+++ leak_hunter.h
@@ -1,14 +1,14 @@
 #ifndef LEAK_HUNTER_H
-#define LEAK_HUNTER_H
+# define LEAK_HUNTER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
-#define OUTPUT_FILE_NAME	"leak_log.txt"
-#define malloc(size)		xmalloc(size, __FILE__, __LINE__)
-#define free(men_ref)		xfree(mem_ref)
+# define OUTPUT_FILE_NAME	"leak_log.txt"
+# define malloc(size)		xmalloc(size, __FILE__, __LINE__)
+# define free(mem_ref)		xfree(mem_ref)
 
 typedef struct s_list
 {
@@ -20,14 +20,15 @@ typedef struct s_list
 }				t_list;
 
 /*list tools*/
-t_list			*ft_lstlast(t_list *lst);
-void			ft_lstadd_back(t_list **alst, t_list *new);
-t_list  		*ft_lstnew(void *address, size_t size, size_t line, char *file);
+t_list			*lstlast(t_list *lst);
+void			lstadd_back(t_list **alst, t_list *new);
+t_list  		*create_node(void *address, size_t size, size_t line, char *file);
+void			delete_node(t_list** head, void *key);
 
 /*core functions*/
 void			*xmalloc(size_t size, char *file, size_t line);
 void			xfree(void *mem_ref);
 void			print_mem_data();
-
+void			fill_data();
 
 #endif /*LEAK_HUNTER_H*/
