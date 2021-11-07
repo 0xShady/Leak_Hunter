747a95277b8e6daa42bdc467c8b19c0e2790bbf2
0xShady <ael-fadi@student.1337.ma>
2021-11-07 20:40:21 +0100

    list tools done

--- leak_hunter.c
+++ leak_hunter.c
@@ -5,13 +5,19 @@
 
 t_list *mem_data = NULL;
 
+void	xfree(void *mem_ref)
+{
+	delete_node(&mem_data, mem_ref);
+	free(mem_ref);
+}
+
 void	*xmalloc(size_t size, char *file, size_t line)
 {
 	void *ptr;
 
 	ptr = malloc(size);
 	if (ptr != NULL)
-		ft_lstadd_back(&mem_data, ft_lstnew(ptr, size, line, file));
+		lstadd_back(&mem_data, create_node(ptr, size, line, file));
 	return (ptr);
 }
 
@@ -27,5 +33,43 @@ void	print_mem_data()
 		printf("file: %s\n", tmp->file);
 		printf("line: %zu\n", tmp->line);
 		tmp = tmp->next;
+	}
+}
+
+void	fill_data()
+{
+	t_list	*tmp;
+	FILE	*log;
+	int		leaks_size;
+	int		allocated_blocks;
+
+	leaks_size = 0;
+	allocated_blocks = 0;
+	if ((log = fopen(OUTPUT_FILE_NAME, "w")))
+	{
+		tmp = mem_data;
+		while (tmp)
+		{
+			allocated_blocks++;
+			leaks_size += tmp->size;
+			tmp = tmp->next;
+		}
+		fprintf(log, "┌─────────┤ LEAKS LOG ├─────────┐\n");
+		fprintf(log, "│ Total leaks found: %10d │\n", allocated_blocks);
+		fprintf(log, "│ Total leaks size: %11d │\n", leaks_size);
+		fprintf(log, "├───────────────────────────────┤\n");
+		tmp = mem_data;
+		while (tmp)
+		{
+			fprintf(log, "│ address: %20p │\n", tmp->address);
+			fprintf(log, "│ size (bytes): %15zu │\n", tmp->size);
+			fprintf(log, "│ file: %23s │\n", tmp->file);
+			fprintf(log, "│ line: %23zu │\n", tmp->line);
+			if (tmp->next)
+				fprintf(log, "├───────────────────────────────┤\n");
+			tmp = tmp->next;
+		}
+		fprintf(log, "└───────────────────────────────┘\n");
+		fclose(log);
 	}
 }
